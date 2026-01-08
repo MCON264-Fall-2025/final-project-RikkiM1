25b56d5aec38831df43cb8332ce059d3393c35ac
Rikkimann <rikkimann1234@gmail.com>
2026-01-08 18:47:39 -0500

    trying

--- src/main/java/edu/course/eventplanner/service/GuestListManager.java
+++ src/main/java/edu/course/eventplanner/service/GuestListManager.java
@@ -17,6 +17,7 @@ public class GuestListManager {
     //method to add guests
     public void addGuest(Guest guest) {
         //check if the guest already exists
+
         String key = (guest.getName() + "-" + guest.getGroupTag()).toLowerCase();
         if (guestsByName.containsKey(key)) {
             System.out.println("Guest already exists: " + guest.getName());
@@ -30,29 +31,29 @@ public class GuestListManager {
 
     public boolean removeGuest(String guestKey) {
 
+        if (guestKey.contains("-")) {
+            //split the guestkey into name and tag
+            String[] parts = guestKey.split("-", 2); // split only on first dash
+            String name = parts[0];
+            String tag = parts[1];
 
-        //split the guestkey into name and tag
-        String[] parts = guestKey.split("-", 2); // split only on first dash
-        String name = parts[0];
-        String tag = parts[1];
 
+            Guest guest = guestsByName.get(guestKey.toLowerCase());
 
-        Guest guest = guestsByName.get(guestKey.toLowerCase());
-
-        // remove by name and tag only
-if(guest==null){
-    return false;
-}
-        if (guest.getName().equalsIgnoreCase(name)) {
-            if (guest.getGroupTag().equalsIgnoreCase(tag)) {
-                guests.remove(guest);
-                guestsByName.remove(guestKey);
-                return true;
+            // remove by name and tag only
+            if (guest == null) {
+                return false;
+            }
+            if (guest.getName().equalsIgnoreCase(name)) {
+                if (guest.getGroupTag().equalsIgnoreCase(tag)) {
+                    guests.remove(guest);
+                    guestsByName.remove(guestKey);
+                    return true;
+                }
             }
         }
-
-        return false; // not found
-    }
+            return false; // not found
+        }
 
 
     public Guest findGuest(String guestName) {
