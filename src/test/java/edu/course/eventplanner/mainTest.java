1da118dc6f4d377edb2a1ae161fd5cf1e6196bd6
Rikkimann <rikkimann1234@gmail.com>
2026-01-09 00:06:40 -0500

    l

---
+++ src/test/java/edu/course/eventplanner/mainTest.java
@@ -0,0 +1,17 @@
+package edu.course.eventplanner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class mainTest {
+
+    @Test
+    void testMainRunsWithoutCrashing() {
+        try {
+            Main.main(new String[]{});
+        } catch (Exception e) {
+            fail("Main method should run without throwing an exception");
+        }
+    }
+}
